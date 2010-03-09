550fe41074e05d628789f72f0cf377aa127c0c2f
Peter TB Brett <peter@peter-b.co.uk>
2010-03-09 09:56:12 +0000

    scm: Apply a box filter over an array.

--- scm/ssc/improc/integral-image.scm
+++ scm/ssc/improc/integral-image.scm
@@ -1,5 +1,6 @@
 (define-module (ssc improc integral-image)
-  #:export (make-integral-image box-integral))
+  #:use-module (ice-9 optargs)
+  #:export (make-integral-image box-integral apply-box-filter!))
 
 ;;;; array-index-map-in-order! array proc
 ;;
@@ -72,3 +73,37 @@
             (array-ref-or-0 II x1 y1))
          (+ (array-ref-or-0 II x1 y2)
             (array-ref-or-0 II x2 y1))))))
+
+(define* (apply-box-filter! src-array dest-array
+                            #:key (mapfunc list)
+                            #:rest box-specs)
+  (let* ((dest-bounds (array-shape dest-array))
+         (dest-lower (map car dest-bounds))
+         (dest-upper (map cadr dest-bounds))
+         (src-lower (apply mapfunc dest-lower))
+         (src-upper (apply mapfunc dest-upper))
+         (stride (map / (map - src-upper src-lower)
+                        (map - dest-upper dest-lower)))
+         (offset (map (lambda (src dest stride) (- src (* dest stride)))
+                      src-lower dest-lower stride)))
+
+    (define (box-integral-at-point point spec)
+      (let ((lower-bounds (list-ref spec 0))
+            (upper-bounds (list-ref spec 1)))
+        (box-integral src-array
+                      (map + point lower-bounds)
+                      (map + point upper-bounds))))
+
+    (define (box-filter-at-point . dest-point)
+      (let ((src-point (map (lambda (d s o) (+ (* d s) o))
+                            dest-point stride offset)))
+
+        (let lp ((v 0)
+                 (lst box-specs))
+          (if (null? lst)
+              v
+              (lp (+ v (box-integral-at-point src-point (car lst)))
+                  (cdr lst))))))
+
+    (array-index-map-in-order! dest-array box-filter-at-point)
+    dest-array))
--- scm/unit-tests/t0002-integral-image.scm
+++ scm/unit-tests/t0002-integral-image.scm
@@ -13,3 +13,15 @@
   (assert-equal 4 (box-integral I '(0 0) '(1 1)))
   (assert-equal 4 (box-integral I '(-1 -1) '(1 1)))
   (assert-equal 4 (box-integral I '(0 0) '(2 2))))
+
+(begin-test 'apply-box-integral!
+  (define II (make-integral-image (make-array 1 3 3)))
+  (let ((D (make-array 0 3 3))
+        (expected #2((4 6 4) (6 9 6) (4 6 4))))
+    (assert-equal expected
+                  (apply-box-filter! II D '((-1 -1) (1 1)))))
+  (let ((D (make-array 0 3 3))
+        (filtspec '(((-1 -1) (-1 1)) ((1 -1) (1 1))))
+        (expected #2((2 3 2) (4 6 4) (2 3 2))))
+    (assert-equal expected (apply apply-box-filter! II D
+                                  filtspec))))
