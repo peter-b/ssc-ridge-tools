74fe8c5578613582728493e7a98dc64104a75b22
Peter TB Brett <peter@peter-b.co.uk>
2010-03-11 15:51:57 +0000

    scm: Nicer algorithm for array-index-map-in-order!

--- scm/ssc/arrays.scm
+++ scm/ssc/arrays.scm
@@ -13,35 +13,14 @@
 ;; by A and (x1 - 1, x2 - 1, ..., xn - 1) will have been previously
 ;; visited.
 (define (array-index-map-in-order! array proc)
-  (define B (array-shape array))
-
-  ;; Calculates the indices of the next entry to run proc for. Returns
-  ;; #f if all the locations in the array have been visited.
-  (define (next-indices! current)
-    ;; ilst is the current offset into the indices list, and blst is
-    ;; the current offset into the bounds list.
-    (let lp ((ilst current)
-             (blst B))
-      (if (null? ilst)
-          ;; If we get to the end of the indices list without finding
-          ;; an index that we're able to increment, we must be
-          ;; finished.
-          #f
-          ;; If the current index is at its upper bound, set it to its
-          ;; lower bound and carry on to the next index. Otherwise,
-          ;; increment it, and return the modified indices.
-          (let ((save-idx (car ilst)))
-            (if (= save-idx (cadr (car blst)))
-                (begin
-                  (set-car! ilst (car (car blst)))
-                  (lp (cdr ilst) (cdr blst)))
-                (begin
-                  (set-car! ilst (1+ (car ilst)))
-                  current))))))
-
-  (let lp ((i (map car B))) ; Initialise the indices to all lower
-                            ; bounds.
-    (apply array-set! array (apply proc i) i)
-    (if (next-indices! i)
-        (lp i)
-        #t)))
+  (define (map-recursive indices shape)
+    (if (null? shape)
+        (apply array-set! array (apply proc indices) indices)
+        (let ((dim-bounds (car shape)))
+          (let ((lower (car dim-bounds))
+                (upper (cadr dim-bounds)))
+            (do ((i lower (1+ i)))
+                ((> i upper)
+                 array)
+              (map-recursive (cons i indices) (cdr shape)))))))
+  (map-recursive '() (reverse (array-shape array))))
