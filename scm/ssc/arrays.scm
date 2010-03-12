f69f953f4d593046f19a6799be1d7c99c35d0b30
Peter TB Brett <peter@peter-b.co.uk>
2010-03-12 16:56:04 +0000

    scm: Add array-index-for-each.
    Implement array-index-map-in-order! in terms of array-index-for-each.

--- scm/ssc/arrays.scm
+++ scm/ssc/arrays.scm
@@ -2,6 +2,24 @@
   #:use-module (srfi srfi-1)
   #:export (array-index-map-in-order!))
 
+;;;; array-index-for-each array proc
+;;
+;; Apply proc to each index in array. The vaue returned is
+;; unspecified. Each call is `(PROC I1 ... IN)', where I1...IN is an
+;; index into ARRAY.
+(define (array-index-for-each array proc)
+  (define (map-recursive indices shape)
+    (if (null? shape)
+        (apply proc indices)
+        (let ((dim-bounds (car shape)))
+          (let ((lower (car dim-bounds))
+                (upper (cadr dim-bounds)))
+            (do ((i lower (1+ i)))
+                ((> i upper)
+                 array)
+              (map-recursive (cons i indices) (cdr shape)))))))
+    (map-recursive '() (reverse (array-shape array))))
+
 ;;;; array-index-map-in-order! array proc
 ;;
 ;; See documentation for array-index-map! for usage. The main
@@ -13,14 +31,7 @@
 ;; by A and (x1 - 1, x2 - 1, ..., xn - 1) will have been previously
 ;; visited.
 (define (array-index-map-in-order! array proc)
-  (define (map-recursive indices shape)
-    (if (null? shape)
-        (apply array-set! array (apply proc indices) indices)
-        (let ((dim-bounds (car shape)))
-          (let ((lower (car dim-bounds))
-                (upper (cadr dim-bounds)))
-            (do ((i lower (1+ i)))
-                ((> i upper)
-                 array)
-              (map-recursive (cons i indices) (cdr shape)))))))
-  (map-recursive '() (reverse (array-shape array))))
+  (array-index-for-each
+   array
+   (lambda indices
+     (apply array-set! array (apply proc indices) indices))))
