f1c5f1f59300c61b1beb36c3ff9faef83490d8b7
Peter TB Brett <peter@peter-b.co.uk>
2011-05-12 15:47:46 +0100

    Remove everything other than C version.
    The Scheme, MATLAB and Python implementations are much slower & don't
    support the ridge file format.

--- scm/unit-tests/t0005-kernels.scm
+++
@@ -1,6 +0,0 @@
-(use-modules (ssc improc kernels))
-
-;; This test probably needs to be expanded.
-(begin-test 'dss-kernel
-  (assert-equal #f64@-1(1/6 2/3 1/6) (dss-kernel 1/3 0))
-  (assert-equal #f64@-1(1/2 0 -1/2) (dss-kernel 0 1)))
